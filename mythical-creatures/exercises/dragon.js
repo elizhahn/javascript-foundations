87bf63a50217ba4e2a1b332ed13334b35742fd37
Elizabeth Hahn <elizhahn87@gmail.com>
2020-12-07 18:22:56 -0700

    Added Dragon class, passed tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+var dragonEats = [];
 
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+    if(dragonEats.length < 2) {
+    dragonEats.push("yum");
+  } else {
+    this.hungry = false;
+  }
+}
+}
+
+
+module.exports = Dragon;
